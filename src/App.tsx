898122273600d5a89e142c72ed392440e4ce1446
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-11-26 17:39:13 -0300

    Fast Commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     // w-screen h-screen
     <div className="flex flex-col items-center py-20 px-2 gap-20 bg-black text-white font-Bodoni">
-      <section className="flex items-center justify-center flex-col text-center gap-5 px-5 sm:flex-col sm:text-start md:flex-col lg:flex-row">
+      <section className="flex items-center justify-center flex-col text-center gap-5 px-5 sm:flex-col sm:text-start md:flex-col lg:flex-row">,
         <div className='flex flex-col gap-5 max-w-[390px] sm:max-w-[560px] md:max-w-[650px]'>
           <h2 className='flex flex-col items-center justify-center border-b-4 border-white'><img src={title1} alt="" /></h2>
 
@@ -46,7 +46,9 @@ function App() {
             </div>
           </div>
 
-          <button className='font-sans font-bold text-4xl w-[235px] h-[60px] rounded-xl text-[#00C2CB] bg-white hover:bg-zinc-900 hover:border-2 hover:border-white transition sm:text-5xl sm:w-[80%] sm:h-[90px] '>EU QUERO!</button>
+          <button className='font-sans font-bold text-4xl w-[235px] h-[60px] rounded-xl text-[#00C2CB] bg-white hover:bg-zinc-900 hover:border-2 hover:border-white transition sm:text-5xl sm:w-[80%] sm:h-[90px]'>
+            <a href="https://pay.kiwify.com.br/9hZC35j" target="_blank">EU QUERO!</a>
+          </button>
         </div>
 
         <div>
@@ -54,14 +56,26 @@ function App() {
         </div>
       </section>
 
-      <section className='w-[100px] h-[100px] bg-red-900 rounded'>
-        <div><img src="" alt="" /></div>
+      <section className='flex flex-col items-center justify-center gap-10 text-center rounded'>
+        <div className='w-[270px] h-[200px] rounded-3xl border-[1px] sm:rounded-[40px] sm:w-[560px] sm:h-[330px] md:w-[750px] md:h-[430px]'>
+          <iframe className='w-full h-full rounded-[40px]' src="https://www.youtube.com/embed/Qil2r2g1q2g" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
 
-        <p>assista ao vídeo para entender melhor</p>
+        <p className='text-2xl w-[300px] sm:w-[500px] sm:text-5xl'>assista ao vídeo para entender melhor</p>
       </section>
 
       <section className='w-[100px] h-[100px] bg-red-900 rounded'>
+        <img src="" alt="" />
 
+        <div>
+          <div>
+            Ao adquirir este produto na Black Friday, você terá acesso a: PRESETS PREMY + BÔNUS + 60% OFF + SUPORTE
+          </div>
+
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
       </section>
 
       <section className='w-[100px] h-[100px] bg-red-900 rounded'>
